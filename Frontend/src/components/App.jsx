c98145ad484b8cc41e971fef6e3974f3ab5e58be
King-Clawthorne <cohenthomas@wentworth.school.nz>
2026-07-27 10:19:24 +1200

    add equipment pages

--- Frontend/src/components/App.jsx
+++ Frontend/src/components/App.jsx
@@ -7,6 +7,8 @@ import HomePage from '@/pages/HomePage.jsx';
 // first time its route is visited.
 const SignInPage = lazy(() => import('@/pages/SignInPage.jsx'));
 const SignOutPage = lazy(() => import('@/pages/SignOutPage.jsx'));
+const SignInEquipmentPage = lazy(() => import('@/pages/SignInEquipmentPage.jsx'));
+const SignOutEquipmentPage = lazy(() => import('@/pages/SignOutEquipmentPage.jsx'));
 
 export default function App() {
   return (
@@ -15,6 +17,8 @@ export default function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/sign-in" element={<SignInPage />} />
         <Route path="/sign-out" element={<SignOutPage />} />
+        <Route path="/sign-in-equipment" element={<SignInEquipmentPage />} />
+        <Route path="/sign-out-equipment" element={<SignOutEquipmentPage />} />
       </Routes>
     </Suspense>
   );
